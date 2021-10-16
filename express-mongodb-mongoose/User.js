e7126b476240c2acaea30c3018f049ad0c5e039b
Max Carlquist <tenkaklet@gmail.com>
2021-10-16 14:27:59 +0200

    changed to mongoose

--- express-mongodb-mongoose/User.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-    firstName: String,
-    lastName: String
-});
-
-module.exports = mongoose.model('User', userSchema);
